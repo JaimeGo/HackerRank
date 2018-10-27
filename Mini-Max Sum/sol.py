aca1d9e7054ded8eb504cdbbeb83b8975a0a6c1c
Jaime González Ortega <jgonzalez1@uc.cl>
2018-10-26 23:44:46 -0300

    some more

--- Mini-Max Sum/sol.py
+++ Mini-Max Sum/sol.py
@@ -8,6 +8,27 @@ import sys
 
 # Complete the miniMaxSum function below.
 def miniMaxSum(arr):
+    min_sum=0
+    max_sum=0
+
+    min_arr=arr.copy()
+    max_arr=arr.copy()
+
+    for i in range(4):
+        min_num=min(min_arr)
+        max_num=max(max_arr)
+
+        min_sum+=min_num
+        max_sum+=max_num
+
+        min_arr.remove(min_num)
+        max_arr.remove(max_num)
+
+    print(str(min_sum)+" "+str(max_sum))
+
+
+
+
 
 if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))
